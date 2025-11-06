aea91b7534fe0c6d7b2667cef8350c665eb21a2c
^_^ <^@^>
2025-11-06 18:31:50 +0800

    no message

--- src/index.js
+++ src/index.js
@@ -1,59 +1,135 @@
 class URL {
   constructor(url) {
-    this.href = url
-    this.origin = ''
-    this.protocol = ''
-    this.host = ''
-    this.port = ''
-    this.pathname = ''
-    this.search = ''
-    this.hash = ''
-    this.params = {}
-    this.getParams()
+    this._parseURL(url);
   }
-  getParams () {
-    const url = this.href
-    if (!url || typeof url !== 'string') return
-    const m = url.match(/(^https?:)\/\/((\w+\.)?(\w+\.?){2,})/)
-    if (m) {
-      if (m[0]) this.origin = m[0]
-      if (m[1]) this.protocol = m[1]
-      if (m[2]) this.host = m[2]
-    }
-    const pathRegExp = new RegExp(m[0] + '(:(\\d+))?([^\?#]*)?[\?#]*')
-    const pathMatch = url.match(pathRegExp)
-    const searchMatch = url.match(/\?([^#\?]*)?/g)
-    const hashMatch = url.match(/#([^?\?]*)?/g)
 
-    if (pathMatch) {
-      if (pathMatch[2]) this.port = pathMatch[2]
-      if (pathMatch[3]) this.pathname = pathMatch[3]
+  _parseURL(url) {
+    // 基础正则匹配协议、主机、路径等
+    const urlRegex = /^(([^:\/?#]+):)?(\/\/([^\/?#]*))?([^?#]*)(\?([^#]*))?(#(.*))?/;
+    const match = url.match(urlRegex);
+    
+    if (!match) {
+      throw new Error('Invalid URL');
     }
 
-    if (hashMatch) {
-      const arr = []
-      hashMatch.forEach((h,i) => {
-        const hash = h.substring(1)
-        hash.replace(/([^=&]+)?=([^=&]+)?/g, (_, a, b) => {
-          this.params[a] = b
-        })
-        arr.push(i === 0 ? h : h.replace('#', '&'))
-      })
-      this.hash = arr.join('')
+    // 解析各个部分
+    this.protocol = match[2] || '';
+    this.host = match[4] || '';
+    this.pathname = match[5] || '/';
+    this.search = match[6] || '';
+    this.hash = match[8] || '';
+    this.searchParams = new URLSearchParams(this.search.slice(1));
+
+    // 进一步解析主机部分
+    if (this.host) {
+      const hostParts = this.host.split(':');
+      this.hostname = hostParts[0];
+      this.port = hostParts[1] || '';
+    } else {
+      this.hostname = '';
+      this.port = '';
     }
 
-    if (searchMatch) {
-      const arr = []
-      searchMatch.forEach((s, i) => {
-        const search = s.substring(1)
-        search.replace(/([^=&]+)?=([^=&]+)?/g, (_, a, b) => {
-          this.params[a] = b
-        })
-        arr.push(i === 0 ? s : s.replace('?', '&'))
-      })
-      this.search = arr.join('')
-    }
+    // 构建 origin
+    this.origin = this.protocol && this.host ? 
+      `${this.protocol}//${this.host}` : '';
+  }
+
+  toString() {
+    let result = '';
+    if (this.protocol) result += `${this.protocol}:`;
+    if (this.host) result += `//${this.host}`;
+    result += this.pathname;
+    result += this.search;
+    result += this.hash;
+    return result;
+  }
+
+  toJSON() {
+    return this.toString();
   }
 }
 
-export default URL
+// URLSearchParams 的简单实现
+class URLSearchParams {
+  constructor(init = '') {
+    this._params = new Map();
+    
+    if (typeof init === 'string') {
+      this._parseString(init);
+    } else if (init instanceof URLSearchParams) {
+      init.forEach((value, key) => {
+        this.append(key, value);
+      });
+    } else if (init && typeof init === 'object') {
+      Object.entries(init).forEach(([key, value]) => {
+        this.append(key, value);
+      });
+    }
+  }
+
+  _parseString(search) {
+    if (search.startsWith('?')) {
+      search = search.slice(1);
+    }
+    
+    const pairs = search.split('&');
+    pairs.forEach(pair => {
+      const [key, value = ''] = pair.split('=');
+      if (key) {
+        this.append(decodeURIComponent(key), decodeURIComponent(value));
+      }
+    });
+  }
+
+  append(key, value) {
+    const current = this._params.get(key);
+    if (current) {
+      current.push(String(value));
+    } else {
+      this._params.set(key, [String(value)]);
+    }
+  }
+
+  delete(key) {
+    this._params.delete(key);
+  }
+
+  get(key) {
+    const values = this._params.get(key);
+    return values ? values[0] : null;
+  }
+
+  getAll(key) {
+    return this._params.get(key) || [];
+  }
+
+  has(key) {
+    return this._params.has(key);
+  }
+
+  set(key, value) {
+    this._params.set(key, [String(value)]);
+  }
+
+  forEach(callback) {
+    this._params.forEach((values, key) => {
+      values.forEach(value => {
+        callback(value, key, this);
+      });
+    });
+  }
+
+  toString() {
+    const pairs = [];
+    this._params.forEach((values, key) => {
+      values.forEach(value => {
+        pairs.push(`${encodeURIComponent(key)}=${encodeURIComponent(value)}`);
+      });
+    });
+    return pairs.length > 0 ? `?${pairs.join('&')}` : '';
+  }
+}
+
+
+export default URL;
---
+++ test.js
@@ -0,0 +1,5 @@
+import URL from './src/index.js'
+
+const url = new URL('https://www.aaa.com:8989/bbb/ccc/ddd.html?e=1&f=2&g#h=3&i=4&j?k=5#l=6&e=4')
+
+console.log(1001, JSON.stringify(url));
